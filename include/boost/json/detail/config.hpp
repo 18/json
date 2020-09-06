7010113453382c206b97b9be8a0b0a7b4082d4d4
Krystian Stasiowski <sdkrystian@gmail.com>
2020-09-05 23:05:38 -0400

    Fix BOOST_FORCEINLINE in standalone

--- include/boost/json/detail/config.hpp
+++ include/boost/json/detail/config.hpp
@@ -88,6 +88,8 @@
 #ifndef BOOST_FORCEINLINE
 # ifdef _MSC_VER
 #  define BOOST_FORCEINLINE __forceinline
+# elif defined(__GNUC__) || defined(__clang__)
+#  define BOOST_FORCEINLINE inline __attribute__((always_inline))
 # else
 #  define BOOST_FORCEINLINE inline
 # endif
